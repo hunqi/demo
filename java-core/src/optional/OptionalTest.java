d1cd98123cb6a4e44d39b7c99561e761c8458adb
ray.sun <ray.sun@homecredit.cn>
2018-09-25 18:00:49 +0800

    java core II > chapter1 stream: Optional API

--- java-core/src/optional/OptionalTest.java
+++ java-core/src/optional/OptionalTest.java
@@ -1,14 +1,44 @@
 package optional;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
 /**
  * This example demonstrates Optional API
  */
 public class OptionalTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String contents = new String(Files.readAllBytes(Paths.get("test_data.txt")), StandardCharsets.UTF_8);
+        List<String> wordList = Arrays.asList(contents.split("\\PL+"));
+        Optional<String> optionalValue = wordList.stream().filter(s -> s.contains("深圳")).findFirst();
+        System.out.println(optionalValue.orElse("Not word") + " contains 深圳");
+        Optional<String> optionalString = Optional.empty();
+        String result = optionalString.orElse("N/A");
+        System.out.println("result: " + result);
+        result = optionalString.orElseGet(() -> System.getProperty("user.dir"));
+        System.out.println("result: " + result);
+        try {
+            result = optionalString.orElseThrow(IllegalStateException::new);
+        }catch (Throwable e){
+            e.printStackTrace();
+        }
+
+        optionalString = wordList.stream().filter(s -> s.contains("北京")).findFirst();
+        optionalString.ifPresent(s -> System.out.println(s + " contains 北京"));
+
+        Set<String> results = new HashSet<>();
 
     }
 
+    static Optional<Double> inverse(Double x) {
+        return x == 0 ? Optional.empty() : Optional.of(1 / x);
+    }
 
-
+    static Optional<Double> squareRoot(Double x) {
+        return x < 0 ? Optional.empty() : Optional.of(Math.sqrt(x));
+    }
 }
