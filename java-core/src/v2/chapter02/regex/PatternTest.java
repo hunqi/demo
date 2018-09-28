ec0cfc3efdd74e85b0e132b359b2a6adad2b3788
ray.sun <ray.sun@homecredit.cn>
2018-09-28 18:01:05 +0800

    java core II > chapter2 input&output

---
+++ java-core/src/v2/chapter02/regex/PatternTest.java
@@ -0,0 +1,18 @@
+package v2.chapter02.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * example of Pattern
+ */
+public class PatternTest {
+
+    public static void main(String[] args) {
+        Pattern p = Pattern.compile("a*b");
+        Matcher m = p.matcher("aaaaab");
+        boolean b = m.matches();
+        System.out.println("b: " + b);
+    }
+
+}
